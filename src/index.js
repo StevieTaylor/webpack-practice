34c6f4c4417892cc4d79d4c23df5d52ff6390e45
StevieTaylor <galei111@126.com>
2021-07-09 17:13:54 +0800

    hot module reload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 /*
  * @Author: Stevie
  * @Date: 2021-07-09 15:13:57
- * @LastEditTime: 2021-07-09 15:18:35
+ * @LastEditTime: 2021-07-09 17:13:10
  * @LastEditors: Stevie
  * @Description: 
  */
 class Main{
   constructor(){
-    document.write('this is main function')
+    document.write('this is main')
   }
 }
 new Main();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Stevie
  * @Date: 2021-07-09 15:14:31
- * @LastEditTime: 2021-07-09 16:52:32
+ * @LastEditTime: 2021-07-09 17:12:18
  * @LastEditors: Stevie
  * @Description:
  */
@@ -9,6 +9,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -30,4 +31,18 @@ module.exports = {
       scriptLoading: 'blocking',
     }),
   ],
+  devServer:{
+    port: 7090,
+    hot:true,
+    compress: true,
+    stats: true,
+    proxy: {
+      '/api': {
+        target: 'http://0.0.0.0:80',
+        pathRewrite: {
+          '/api': '',
+        },
+      },
+    }
+  }
 }
