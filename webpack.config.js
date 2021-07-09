eda0cb440ce63b1dbb6b2c30dedb81b4c3337ee7
StevieTaylor <galei111@126.com>
2021-07-09 16:54:51 +0800

    在浏览器中查看效果

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,33 @@
 /*
  * @Author: Stevie
  * @Date: 2021-07-09 15:14:31
- * @LastEditTime: 2021-07-09 15:32:59
+ * @LastEditTime: 2021-07-09 16:52:32
  * @LastEditors: Stevie
- * @Description: 
+ * @Description:
  */
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports={
+module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname,'dist'),
-    filename: 'bundle.[contenthash:8].js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash:8].js',
   },
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
-        test:/\.(js|jsx)$/,
+        test: /\.(js|jsx)$/,
         use: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
-  }
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      inject: 'body',
+      scriptLoading: 'blocking',
+    }),
+  ],
 }
